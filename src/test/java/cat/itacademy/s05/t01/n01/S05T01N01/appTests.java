d52ac92372fa76bb20957cfa04054858ad9e15ff
adriaVH <adriavargashernandez@gmail.com>
2025-07-24 12:23:01 +0200

    gitignore allowed target

---
+++ src/test/java/cat/itacademy/s05/t01/n01/S05T01N01/appTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s05.t01.n01.S05T01N01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class appTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
